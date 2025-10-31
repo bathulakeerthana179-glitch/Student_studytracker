3120c93df3bb0528a9d0e05095e57aada06a1379
Keerthana Bathula <bathulakeerthana179@gmail.com>
2025-10-31 22:01:03 +0530

    mycommit

--- Studytrack/settings.py
+++ Studytrack/settings.py
@@ -111,8 +111,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'studytrackerai@gmail.com'
-EMAIL_HOST_PASSWORD = 'zvbmcdmoccwbtcty'
+EMAIL_HOST_USER =  os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
 DEFAULT_FROM_EMAIL = 'StudyTrack <noreply@studytrack.com>'
 
 # AI Configuration (Optional)
